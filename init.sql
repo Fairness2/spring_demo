d3f1f0e4133d080861690aa76f70efdb52c917bf
kuzminykh <geve95@gmail.com>
2021-07-08 23:01:53 +0500

    lesson_144414 / Заготовки

--- init.sql
+++ init.sql
@@ -1,5 +1,12 @@
 BEGIN;
-DELETE FROM product;
+
+CREATE TABLE `product` (
+                           `id` int NOT NULL AUTO_INCREMENT,
+                           `title` varchar(255) DEFAULT NULL,
+                           `cost` int DEFAULT NULL,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 INSERT INTO product (`id`, `title`, `cost`) VALUES
 (1, 'Some 1', 100),
 (2, 'Some 2', 200),
@@ -11,4 +18,29 @@ INSERT INTO product (`id`, `title`, `cost`) VALUES
 (8, 'Some 8', 800),
 (9, 'Some 9', 900),
 (10, 'Some 10', 1000);
+
+CREATE TABLE `user` (
+                        `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                        `name` varchar(255) NOT NULL,
+                        PRIMARY KEY (`id`),
+                        UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO user (name)
+VALUES ('Вася'), ('Петя');
+
+CREATE TABLE `ref_user_product` (
+                                    `id` int NOT NULL AUTO_INCREMENT,
+                                    `user_id` bigint unsigned DEFAULT NULL,
+                                    `product_id` int DEFAULT NULL,
+                                    `current_cost` int DEFAULT NULL,
+                                    PRIMARY KEY (`id`),
+                                    KEY `ref_user_product_user_id_fk` (`user_id`),
+                                    KEY `ref_user_product_product_id_fk` (`product_id`),
+                                    CONSTRAINT `ref_user_product_product_id_fk` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+                                    CONSTRAINT `ref_user_product_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
 COMMIT;
